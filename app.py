5a4981b80282b51d99ac57a9f49bd6f4adca1bbd
skygravity <skygravity8@gmail.com>
2019-11-16 20:06:02 +0200

    New py file app

---
+++ app.py
@@ -0,0 +1,11 @@
+print('Vytautas')
+print('o----')
+print(' ||||')
+print('*' * 10)
+
+# variables - used to store data into computers memory;
+# example;
+price = 10
+# python will alocate some memory and store data - "price";
+print(price) #use without quatation " ; output should be 10;
+
