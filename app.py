c90d62f04827e251862d42cabb4eae7f27774adb
skygravity <skygravity8@gmail.com>
2019-12-02 20:30:35 +0200

    paths

--- app.py
+++ app.py
@@ -1,13 +1,4 @@
-from ecommerce import shipping
-import random
+from pathlib import Path
 
-
-class Dice:
-        def roll(self):
-                first = random.randint(1, 6)
-                second = random.randint(1, 6)
-                return first, second
-
-
-dice = Dice()
-print(dice.roll())
+path = Path("emails")
+print(path.mkdir())
